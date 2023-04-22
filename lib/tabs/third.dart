778908f64d519d3cad098aadf56a7342bea55300
dtb4life <aveok4now@gmail.com>
2023-04-22 14:23:26 +0300

    second tab page

---
+++ lib/tabs/third.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThirdTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Заключенных договоров пока нет',
+        style: TextStyle(
+          fontSize: 20.0,
+        ),
+      ),
+    );
+  }
+}
