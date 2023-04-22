9593268b17e0fec10ae3d93e695f511c3108f24b
dtb4life <aveok4now@gmail.com>
2023-04-22 14:23:37 +0300

    Agent's page

---
+++ lib/requests.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+
+class InsuranceRequest {
+  int id;
+  String firstName;
+  String lastName;
+  String email;
+  String type;
+  int salary;
+
+  InsuranceRequest({required this.id, required this.firstName, required this.lastName, required this.email,required this.type, required this.salary,});
+}
+
+class InsuranceRequestsList extends StatefulWidget {
+  @override
+  _InsuranceRequestsListState createState() => _InsuranceRequestsListState();
+}
+
+class _InsuranceRequestsListState extends State<InsuranceRequestsList> {
+  List<InsuranceRequest> requests = [
+    InsuranceRequest(
+      id: 1,
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@gmail.com",
+      type: "Заявка на автострахование",
+      salary: 50000,
+    ),
+    InsuranceRequest(
+      id: 2,
+      firstName: "Jane",
+      lastName: "Doe",
+      email: "janedoe@gmail.com",
+      type: "Заявка на страхование здоровья и жизни",
+      salary: 75000,
+    ),
+    InsuranceRequest(
+      id: 3,
+      firstName: "Bob",
+      lastName: "Smith",
+      email: "bobsmith@gmail.com",
+      type: "Заявка на страхование путешествия",
+      salary: 40000,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Список заявок'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: ListView.builder(
+        itemCount: requests.length,
+        itemBuilder: (BuildContext context, int index) {
+          InsuranceRequest request = requests[index];
+          return Card(
+           child: ListTile(
+  title: Text('${request.type} от ${request.firstName} ${request.lastName}'),
+  subtitle: Text('Зарплата клиента: ${request.salary}'),
+  trailing: Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      IconButton(
+        icon: Icon(Icons.thumb_up),
+        color: Colors.green,
+        onPressed: () {
+          // Обработчик нажатия на кнопку "Одобрить"
+          setState(() {
+            requests.removeAt(index);
+          });
+        },
+      ),
+      IconButton(
+        icon: Icon(Icons.thumb_down),
+        color: Colors.red,
+        onPressed: () {
+          // Обработчик нажатия на кнопку "Отклонить"
+          setState(() {
+            requests.removeAt(index);
+          });
+        },
+      ),
+    ],
+  ),
+  onTap: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ClientInfoScreen(request: request),
+      ),
+    );
+  },
+)
+
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ClientInfoScreen extends StatelessWidget {
+  final InsuranceRequest request;
+
+  const ClientInfoScreen({Key? key, required this.request}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Информация о клиенте'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Имя: ${request.firstName}'),
+            Text('Фамилия: ${request.lastName}'),
+            Text('Email: ${request.email}'),
+            Text('Тип страхования: ${request.type}'),
+            Text('Зарплата: ${request.salary}'),
+            // Другие поля, если есть
+          ],
+        ),
+      ),
+    );
+  }
+}
