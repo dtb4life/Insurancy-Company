4684e394137cd9f99025ecfb11e108e85de8d43b
dtb4life <aveok4now@gmail.com>
2023-04-23 16:59:35 +0300

    Client's notifications

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,36 @@
 //import 'agent_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 //import 'package:device_preview/device_preview.dart';
 
 import 'interface.dart';
 
-void main() => runApp(
+
+
+void main() {
+  runApp(
         MaterialApp(
+          title: 'Страховая фирма',
           debugShowCheckedModeBanner: false,
           home: InsuranceApp(),
-        )
+          
+        ),
+      
     );
+      AwesomeNotifications().initialize(
+          null,
+          [
+            NotificationChannel(
+              channelKey: 'basic', 
+              channelName: 'Basic notifications', 
+              channelDescription: 'Channel 4 tests')
+          ],
+          debug:true,
+        );
+}
 
 
 
